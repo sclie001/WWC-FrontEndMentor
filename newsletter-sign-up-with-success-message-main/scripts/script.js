6fa6511b9763eee9a20a437444a31636093197ce
sclie001 <daisysc7789@gmail.com>
2024-03-24 10:37:10 -0400

    hide error message when page first loads

---
+++ newsletter-sign-up-with-success-message-main/scripts/script.js
@@ -0,0 +1,6 @@
+const errorMessage = document.getElementById("error-msg");
+const subscribeBtn = document.getElementById("subscribe-btn");
+
+//hide error message until error found
+errorMessage.style.display = 'none';
+
