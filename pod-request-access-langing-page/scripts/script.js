d8c922d92c27e40b4fdd9a3806fdb27d853ee7db
sclie001 <daisysc7789@gmail.com>
2024-04-12 09:57:55 -0400

    created validateEmail function to validate email whether it is empty or does not match email format and present error message

--- pod-request-access-langing-page/scripts/script.js
+++ pod-request-access-langing-page/scripts/script.js
@@ -3,6 +3,19 @@ const btn_req_access = document.getElementById('btn-req-access');
 function validateEmail(e){
     e.preventDefault();
     console.log("IN Validate Email");
+
+    const email = document.getElementById('email-addr');
+    const error_msg = document.getElementById('error-msg');
+
+    if(email.validity.valueMissing || email.validity.typeMismatch){
+        console.log(email.validity.valid);
+        error_msg.textContent = "Oops! Please, check your email";
+        
+        return false;
+    }
+    else{
+        return true;
+    }
 }
 
 btn_req_access.addEventListener('click', validateEmail);
