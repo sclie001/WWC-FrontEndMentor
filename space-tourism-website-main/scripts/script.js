d585af74b3e935f9d21a31e222fff7d5fbf9753d
sclie001 <daisysc7789@gmail.com>
2024-06-08 13:26:49 -0400

    gave primary-navigation data attribute to identify whether it is visible/not and changed its value in Javascript

--- space-tourism-website-main/scripts/script.js
+++ space-tourism-website-main/scripts/script.js
@@ -1,49 +1,69 @@
-navHamburger = document.getElementById("hamburger-menu");
-closeIcon = document.getElementById("close-icon");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const navMenu = document.querySelector(".primary-navigation");
 
-closeIcon.style.display = "none";
+/**helper method */
+function toggle(){
+    toggleNavigation();
+}
+
+/**toggle navigation menu */
+function toggleNavigation(){
+    const menuVisible = navMenu.getAttribute("data-visible");
+    
+    if(menuVisible === "false"){
+        navMenu.setAttribute("data-visible", true);
+    } else{
+        navMenu.setAttribute("data-visible", false);
+    }
+    console.log(menuVisible);
+}
+
+navToggle.addEventListener('click', toggle);
+// closeIcon = document.getElementById("close-icon");
+
+// closeIcon.style.display = "none";
 
 /**convert from hamburger to close icon */
-function hamburgerToClose(){
-    navHamburger.style.display = "none";
-    displayCloseIcon();
+// function hamburgerToClose(){
+//     navToggle.style.display = "none";
+//     displayCloseIcon();
 
-    //show menu links
-    toggleNavigationLinks();
+//     //show menu links
+//     // toggleNavigationLinks();
     
-}
+// }
 
-/**convert from close icon to hamburger */
-function closeToHamburger(){
-    closeIcon.style.display = "none";
-    displayHamburger();
+// /**convert from close icon to hamburger */
+// function closeToHamburger(){
+//     closeIcon.style.display = "none";
+//     displayHamburger();
     
-    //hide menu links
-    toggleNavigationLinks();
-}
+//     //hide menu links
+//     toggleNavigationLinks();
+// }
 
-/**display hamburger */
-function displayHamburger(){
-    navHamburger.style.display = "inline-block";
-}
+// /**display hamburger */
+// function displayHamburger(){
+//     navHamburger.style.display = "inline-block";
+// }
 
-/**display close icon*/
-function displayCloseIcon(){
-    closeIcon.style.display = "inline-block";
-}
+// /**display close icon*/
+// function displayCloseIcon(){
+//     // closeIcon.style.display = "inline-block";
+// }
 
-/**Toggle between showing and hiding navigation menu links */
-function toggleNavigationLinks(){
-    navMenu = document.getElementById("nav-menu");
+// /**Toggle between showing and hiding navigation menu links */
+// function toggleNavigationLinks(){
+//     navMenu = document.getElementById("nav-menu");
 
-    if(navMenu.style.display === 'block'){
-        navMenu.style.display = 'none';
-    }
-    else{
-        navMenu.style.display = 'block';
-    }
-}
+//     if(navMenu.style.display === 'block'){
+//         navMenu.style.display = 'none';
+//     }
+//     else{
+//         navMenu.style.display = 'block';
+//     }
+// }
 
 
-navHamburger.addEventListener('click', hamburgerToClose);
-closeIcon.addEventListener('click', closeToHamburger);
+// navHamburger.addEventListener('click', hamburgerToClose);
+// closeIcon.addEventListener('click', closeToHamburger);
